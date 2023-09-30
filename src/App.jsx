bef3bd18276a22889fbd3590cd92186210317231
Ananthak <Ananthakrishna.K@iiitb.ac.in>
2023-09-30 17:05:28 +0530

    React router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Login from './pages/login'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
   return (
     <>
-      App
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
+    </Router>
     </>
   )
 }
