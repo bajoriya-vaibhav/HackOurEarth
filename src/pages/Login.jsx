c2dbe7f45ea5205b05c86f5ed6e33b6c443b1064
Aaryan-Ajith-Dev <117074091+Aaryan-Ajith-Dev@users.noreply.github.com>
2023-10-01 13:00:39 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,30 @@ import './Login.css'
 import { Link } from 'react-router-dom'
 import Signup from './Signup'
 import FloatingNav from '../components/FloatingNav'
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const [name, setName] = useState('');
+  const [pass, setPass] = useState('');
+  const navigate = useNavigate();
+
+  const checkLogin = async(userData)=>{
+    const isValid =await axios.post('http://localhost:5000/users/login',userData)
+    const check = isValid.data
+    if(check === false){
+      alert('Please check you password');
+    }
+    else if(check){
+      alert('Logged in successfully');
+      navigate('/dashboard');
+    }
+    else{
+      alert('Invalid Username');
+    }
+  }
+  
   return (
     <div className='login'>
       <FloatingNav/>
@@ -13,11 +35,14 @@ const Login = () => {
       <div className='login-text'>
             <h1>Nice to See You!</h1>
             <h2>Enter your email and password to sign in.</h2>
-            <h3>Email</h3>
-            <input type="text" placeholder='Email..'/>
+            <h3>Username</h3>
+            <input type="text" value={name} onChange={() => { setName(event.target.value) }} placeholder='Your Fullname..' />
             <h3>Password</h3>
-            <input type="password" placeholder='Password...' />
-            <button>Sign In</button>
+            <input type="password" value={pass} onChange={() => { setPass(event.target.value) }} placeholder='Your Password..' />
+            <button onClick={() => checkLogin({
+            name: name,
+            password: pass
+          }) }>Login</button>
             <h4>Dont have an account?  <Link to='/signup'>Sign Up</Link> </h4>
       </div>
     </div>
